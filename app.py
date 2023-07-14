469ecdee32767dbbf6fc83000c451a185062f120
Anthony T <tokyographer@gmail.com>
2023-07-14 23:24:47 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+import openai
+import constants
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# API imported from constants.py file
+os.environ["OPENAI_API_KEY"] = constants.APIKEY
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def process_query():
+    prompt = request.form['query']
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=100
+    )
+    answer = response.choices[0].text.strip()
+    return render_template('index.html', answer=answer)
+
+if __name__ == '__main__':
+    app.run(debug=True)
