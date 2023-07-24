744542e9d499eee8071fde930fe9e9adea59a470
Anthony T <tokyographer@gmail.com>
2023-07-24 14:48:01 +0200

    Test port binding in Heroku

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def process_query():
     return render_template('index.html', answer=answer)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
